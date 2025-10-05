3b3b3113147d8c8b3df3e775b0892889f73a0da6
Kartik-jindal <kartikjindal2003@gmail.com>
2025-10-05 11:55:23 +0000

    Day 3: Completed hashing theory; solved Two Sum, Contains Duplicate, Valid Anagram, and Group Anagrams

--- day_003_python_hashing/hashing_concepts.py
+++ day_003_python_hashing/hashing_concepts.py
@@ -16,3 +16,22 @@ class Solution:
             for j in range(i+1, n):
                 if nums[i] + nums[j] == target:
                     return [i,j]
+
+# 242. Valid Anagram
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if sorted(list(s)) == sorted(list(t)):
+            return True
+        else:
+            return False
+        
+# 49. Group Anagrams
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        final = defaultdict(list)
+        for i in strs:
+            key = tuple(sorted(i))
+            final[key].append(i)
+        return list(final.values())
+    
