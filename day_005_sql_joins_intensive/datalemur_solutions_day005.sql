d6209e9bc9a7c4f36ced2ec1268c4d967b66761e
Kartik-jindal <kartikjindal2003@gmail.com>
2025-10-07 18:17:09 +0000

    Day 5: Completed SQL Joins Intensive Bootcamp

---
+++ day_005_sql_joins_intensive/datalemur_solutions_day005.sql
@@ -0,0 +1,31 @@
+-- Highest-Grossing Items
+
+WITH summed_spend AS ( -- Step 1: Sum spend by category and product for 2022
+  SELECT 
+    category, 
+    product, 
+    SUM(spend) AS total_spend
+  FROM product_spend
+  WHERE YEAR(transaction_date) = 2022
+  GROUP BY category, product
+),
+
+ranked AS ( -- Step 2: Rank products within each category by total spend
+  SELECT 
+    category, 
+    product, 
+    total_spend, 
+    ROW_NUMBER() OVER ( -- Window function to rank products by spend within each category 
+      PARTITION BY category -- works like a GROUP BY for window functions 
+      ORDER BY total_spend DESC -- highest spend first
+    ) AS `rank`
+  FROM summed_spend -- taken from the previous made summed_spend CTE by reference 
+)
+
+SELECT 
+  category, 
+  product, 
+  total_spend
+FROM ranked
+WHERE `rank` <= 2
+ORDER BY category, total_spend DESC;
