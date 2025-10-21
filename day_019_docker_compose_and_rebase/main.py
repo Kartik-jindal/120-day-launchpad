666513271385ed86d1a6bc7942b870c37661668b
Kartik-jindal <kartikjindal2003@gmail.com>
2025-10-21 10:02:53 +0000

    FIX: Add comments and correct env var in compose file

--- day_019_docker_compose_and_rebase/main.py
+++ day_019_docker_compose_and_rebase/main.py
@@ -3,6 +3,8 @@ import os
 # Import the FastAPI class to create the web application
 from fastapi import FastAPI
 
+#This is main application
+
 # Create a FastAPI app instance that will handle incoming HTTP requests
 app = FastAPI()
 
